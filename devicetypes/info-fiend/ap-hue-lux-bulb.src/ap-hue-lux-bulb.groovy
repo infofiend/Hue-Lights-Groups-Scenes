85136385841d376245818c129f567d242ff4006a
claytonjn <nummercl@WBSTAFF.wblib.org>
2016-03-23 14:22:29 -0400

    Fix device page not updating.
    Previously had to leave device page and return to see status updates.

--- devicetypes/info-fiend/ap-hue-lux-bulb.src/ap-hue-lux-bulb.groovy
+++ devicetypes/info-fiend/ap-hue-lux-bulb.src/ap-hue-lux-bulb.groovy
@@ -45,11 +45,7 @@ metadata {
 			}
         }
 
-        controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range:"(0..100)") {
-            state "level", action:"switch level.setLevel"
-        }
-		
-		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
         }
 		
